632877ad9922778c212388e99017282ff55fa813
GingerBulman <105761105+GingerBulman@users.noreply.github.com>
2022-09-05 22:32:33 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,12 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     public static int fibonacci(int n)    {
-    	// the Fibonacci sequence, in which each number is the sum of the two preceding ones. 
-    	//The sequence commonly starts from 0 and 1, 
-    	//although some authors omit the initial terms and start the sequence from 1 and 1 or from 1 and 2. 
-    	//Starting from 0 and 1, the next few values in the sequence are: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
-    	//hey are named after the Italian mathematician Leonardo of Pisa, later known as Fibonacci, 
-    	//who introduced the sequence to Western European mathematics in his 1202 book Liber Abaci.
+  
    
     if (n <= 1)
        return n;
